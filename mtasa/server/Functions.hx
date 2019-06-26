9eb62ca6acc97fe74dd5f315e9aefe345a8aa9a4
pawel-miczka <piorunx@gmail.com>
2019-06-26 21:39:47 +0200

    up: added set, get functions typings

--- mtasa/server/Functions.hx
+++ mtasa/server/Functions.hx
@@ -2945,16 +2945,21 @@ Note: This function does not stop the resource immediately, so don't expect that
 	/**
 		This function gets a setting's value, or a group of settings' values, from the settings registry.
 
+		@param settingName The name of the setting you want to get. See setting names for information on settings names.
+
 		@see https://wiki.mtasa.com/wiki/Get
 	**/
-	static function get():Void
+	static function get(settingName:String):Void
 
 	/**
 		This function is used to save arbitrary data under a certain name on the settings registry.It's important to note that set always writes to the settings.xml file, even if get read the value from a resource's meta.xml. This means that the admin can specify settings in the settings.xml that override the resource's defaults, but that the defaults can still be retrieved if need be. As a general principle, resources should not be designed so that the admin is required to modify them, they should be 'black boxes'.
 
+		@param settingName The name of the setting you want to set. See setting names for information on settings names.
+		@param value The value to set the setting to. This can be any Lua data type, except for functions, most userdata (only resources can't be stored) and threads.
+
 		@see https://wiki.mtasa.com/wiki/Set
 	**/
-	static function set():Void
+	static function set(settingName:String, value:Dynamic):Void
 
 	/**
 		This function executes an arbitrary SQL query and returns the result rows if there are any. It allows parameter binding for security (SQL injection is rendered impossible).
