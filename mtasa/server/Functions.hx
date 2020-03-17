a08515e1e1f98a125113882dc98ba85e5ec1bb73
pawel-miczka <piorunx@gmail.com>
2020-03-17 20:31:04 +0100

    up: server-side clothes and body functions

--- mtasa/server/Functions.hx
+++ mtasa/server/Functions.hx
@@ -15,36 +15,6 @@ typedef FetchRemoteOptions = {
 @:native("_G")
 extern class Functions {
 	/**
-		This function is used to get the name of a body part on a player.
-
-		@see https://wiki.mtasa.com/wiki/GetBodyPartName
-	**/
-	static function getBodyPartName():Void;
-
-	/**
-		This function is used to get the texture and model of clothes by the clothes type and index.
-(Scans through the list of clothes for the specific type).This function returns 2 strings, a texture and model respectively, false if invalid arguments were passed to the function.
-
-		@see https://wiki.mtasa.com/wiki/GetClothesByTypeIndex
-	**/
-	static function getClothesByTypeIndex():Void;
-
-	/**
-		This function is used to get the name of a certain clothes type.This function returns a string (the name of the clothes type) if found, false otherwise.
-
-		@see https://wiki.mtasa.com/wiki/GetClothesTypeName
-	**/
-	static function getClothesTypeName():Void;
-
-	/**
-		This function is used to get the clothes type and index from the texture and model.
-(Scans through the list of clothes for the specific type).
-
-		@see https://wiki.mtasa.com/wiki/GetTypeIndexFromClothes
-	**/
-	static function getTypeIndexFromClothes():Void;
-
-	/**
 		This function creates a collision circle. This is a shape that has a position and a radius and infinite height that you can use to detect a player's presence. Events will be triggered when a player enters or leaves it.
 
 		@see https://wiki.mtasa.com/wiki/CreateColCircle
