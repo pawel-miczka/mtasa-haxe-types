259628017ae43830d40148ede809c6bc59afbd04
pawel-miczka <piorunx@gmail.com>
2020-03-22 02:23:23 +0100

    up: added server-side Explosion functions

--- src/mtasa/server/Functions.hx
+++ src/mtasa/server/Functions.hx
@@ -21,13 +21,6 @@ typedef FetchRemoteOptions = {
 @:native("_G")
 extern class Functions {
 	/**
-		Creates an explosion of a certain type at a specified point in the world. If creator is specified, the explosion will occur only in its dimension.
-
-		@see https://wiki.mtasa.com/wiki/CreateExplosion
-	**/
-	static function createExplosion():Void;
-
-	/**
 		Closes a file handle obtained by fileCreate or fileOpen.
 
 		@see https://wiki.mtasa.com/wiki/FileClose
